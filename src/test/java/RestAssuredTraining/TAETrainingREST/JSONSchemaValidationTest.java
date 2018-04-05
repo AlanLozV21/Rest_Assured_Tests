139083d5ca74205c21848e23a751e1c7a28053ae
AlanLozV21 <oscar.lozada@MX-IT01115.globant.com>
2018-04-05 15:55:05 -0500

    Final delivery of Rest Assured Tests

--- src/test/java/RestAssuredTraining/TAETrainingREST/JSONSchemaValidationTest.java
+++ src/test/java/RestAssuredTraining/TAETrainingREST/JSONSchemaValidationTest.java
@@ -1,13 +1,16 @@
 package RestAssuredTraining.TAETrainingREST;
 
 import static io.restassured.RestAssured.*;
-import static io.restassured.module.jsv.JsonSchemaValidator.*;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchema;
 
 import java.nio.file.Paths;
 
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 
 public class JSONSchemaValidationTest {
 	
@@ -32,16 +35,23 @@ public class JSONSchemaValidationTest {
 	@DataProvider(name = "PostValidationTest")
 	public Object[][] createPostValidationData() {
 		return new Object[][] {
-			{ "posts/{id}", "id", "20", "title", "doloribus ad provident suscipit at"},
-			{ "posts/{id}", "id", "50", "title", "repellendus qui recusandae incidunt voluptates tenetur qui omnis exercitationem"},
-			{ "posts/{id}", "id", "100", "title", "at nam consequatur ea labore ea harum"},
-			
+			{ "20", 20, 2, "doloribus ad provident suscipit at", "qui consequuntur ducimus possimus quisquam amet similique\nsuscipit porro ipsam amet\neos veritatis officiis exercitationem vel fugit aut necessitatibus totam\nomnis rerum consequatur expedita quidem cumque explicabo"},
+			{ "50", 50, 5, "repellendus qui recusandae incidunt voluptates tenetur qui omnis exercitationem", "error suscipit maxime adipisci consequuntur recusandae\nvoluptas eligendi et est et voluptates\nquia distinctio ab amet quaerat molestiae et vitae\nadipisci impedit sequi nesciunt quis consectetur"},
+			{ "100", 100, 10, "at nam consequatur ea labore ea harum", "cupiditate quo est a modi nesciunt soluta\nipsa voluptas error itaque dicta in\nautem qui minus magnam et distinctio eum\naccusamus ratione error aut"},
 		};
 	}
 	
 	@Test(dataProvider = "PostValidationTest")
-	public void testPostsValidation(String resource, String idParameter, String idValue, String fieldParameter, String fieldValueExpected) {
-		Assert.assertEquals(get("http://jsonplaceholder.typicode.com/" + resource, idParameter, idValue, fieldParameter), fieldValueExpected);
+	public void testPostsValidation(String resourceParam, int idValue, int userIdValue, String title, String body) {
+		Response response = given().baseUri("http://jsonplaceholder.typicode.com").pathParam("id", resourceParam).when().get("posts/{id}");
+		response.then().log().all();		
+		
+		JsonPath jsonPath = response.jsonPath();
+		
+		Assert.assertEquals(jsonPath.getInt("id"), idValue);
+		Assert.assertEquals(jsonPath.getInt("userId"), userIdValue);
+		Assert.assertEquals(jsonPath.getString("title"), title);
+		Assert.assertEquals(jsonPath.getString("body"), body);
 	}
 	
 	@Test
